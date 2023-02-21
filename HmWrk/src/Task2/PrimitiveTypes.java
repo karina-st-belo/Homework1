bd84e6f900c4f3dba30edda939bc9635c557e9ed
Karina <karina.beloserow@gmail.com>
2023-02-21 18:07:37 +0100

    Task1

---
+++ HmWrk/src/Task2/PrimitiveTypes.java
@@ -0,0 +1,35 @@
+package Task2;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte  byteNumber=4;
+        System.out.println(byteNumber);
+        short shortName=6;
+        System.out.println(shortName);
+        int intNumber=8;
+        System.out.println(intNumber);
+        double doubleName=4.0;
+        System.out.println(doubleName);
+        long longName=10L;
+        System.out.println(longName);
+        float floatName=2.5f;
+        System.out.println(floatName);
+        boolean booleanName=true;
+        System.out.println(booleanName);
+        char charName='d';
+        System.out.println(charName);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
