fcfb2a3ec31af9ca8d63424991ce2d7dcf1ca249
Karina <karina.beloserow@gmail.com>
2023-02-21 18:08:39 +0100

    Task1

---
+++ HmWrk/src/Main2.java
@@ -0,0 +1,21 @@
+public class Main2 {
+    public static void main(String[] args){
+        int intName=8;
+        System.out.println(intName);
+        char charName='a';
+        System.out.println(charName);
+        boolean booleanName=false;
+        System.out.println(booleanName);
+        double doubleName=6.5;
+        System.out.println(doubleName);
+        short shortName=8;
+        System.out.println(shortName);
+        long longName=12L;
+        System.out.println(longName);
+        float floatName=10.5f;
+        System.out.println(floatName);
+        byte byteName=8;
+        System.out.println(byteName);
+    }
+
+}
